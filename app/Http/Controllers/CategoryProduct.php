f8eb17247b27555ee16416ab4ef51075b6765d22
Nguyen Huu Quan <47737428+nhqtdv@users.noreply.github.com>
2019-11-14 07:47:49 +0700

    Initial commit

---
+++ app/Http/Controllers/CategoryProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+// use App\Http\Request;
+use Session;
+use Illuminate\Support\Facades\Redirect;
+session_start();
+
+class CategoryProduct extends Controller
+{
+    public function add_category_product(){
+    	return view('admin.add_category_product');
+
+    }
+    public function all_category_product(){
+    	$all_category_product = DB::table('tbl_category_product')->get();
+    	$manager_category_product = view('admin.all_category_product')->with('all_category_product',$all_category_product);
+    	return view('admin_layout')->with('admin.all_category_product',$manager_category_product);
+
+    }
+    public function save_category_product(Request $request){
+    	$data = array();
+    	$data['category_name'] = $request->category_product_name;
+    	$data['category_desc'] = $request->category_product_desc;
+    	$data['category_status'] = $request->category_product_status;
+
+    	DB::table('tbl_category_product')->insert($data);
+    	Session::put('message','Thêm danh mục thành công !');
+    	return Redirect::to('all-category');
+    }
+}
